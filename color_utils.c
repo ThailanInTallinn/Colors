d7712fba076851149d2721005975b0d435876d3f
Thailan Gouvêa de Menezes <thailangm@icloud.com>
2026-02-20 12:22:47 +0200

    Feature: makes a given color brighter

--- color_utils.c
+++ color_utils.c
@@ -1,4 +1,7 @@
 #include "color_utils.h"
+#include <stdbool.h>
+#include <stdio.h>
+
 
 struct color make_color(int red, int green, int blue){
 	int new_red = red < 0 ? 0 : red > 255 ? 255 : red;
@@ -6,4 +9,29 @@ struct color make_color(int red, int green, int blue){
 	int new_blue = blue < 0 ? 0 : blue > 255 ? 255 : blue;
 
 	return ((struct color) {.red = new_red, .green = new_green, .blue = new_blue});
+}
+
+int getRed(struct color c){
+	return c.red;
+}
+
+bool equal_color(struct color color1, struct color color2){
+	return (color1.red == color2.red && color1.green ==  color2.green && color1.blue == color2.blue);
+}
+
+struct color brighter(struct color c){
+	int *ptr, temp = 0;
+
+	if((c.red + c.green + c.blue) == 0)
+		return ((struct color) {.red = 3, .green = 3, .blue = 3});
+
+	for(ptr = &c; ptr != &c.blue + 1; ptr++){
+		if(*ptr > 0 && *ptr < 3)
+			*ptr = 3;
+		temp = *ptr / 0.7;
+		if(temp > 255)
+			temp = 255;
+		*ptr = temp;
+	}
+	return c;
 }
--- color_utils.h
+++ color_utils.h
@@ -1,5 +1,6 @@
 #ifndef COLORS_UTILS_H
 #define COLORS_UTILS_H
+#include <stdbool.h>
 
 	struct color {
 		int red;
@@ -8,6 +9,9 @@
 	};
 
 struct color make_color(int, int, int);
+int getRed(struct color);
+bool equal_color(struct color, struct color);
+struct color brighter(struct color);
 
 
 #endif
